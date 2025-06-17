a049969b4d69073cb6342fb89672e11fb0363112
nancy291013 <rakeshgupta212188@gmail.com>
2025-06-17 19:25:25 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,19 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'Bitdefender Login Account'
 author = 'Your Name'
 release = '1.0'
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
-html_static_path = ['_static']    //
+html_theme = 'alabaster'  # You can change to 'sphinx_rtd_theme' if you add it in requirements
+html_static_path = ['_static']
+master_doc = 'index'
